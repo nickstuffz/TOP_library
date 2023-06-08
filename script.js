5de746593ea83e202692dc7764189d5f039f4828
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2023-06-08 12:42:43 -0700

    Progress on book object logic

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    return;
-}
-
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-    return;
-}
-
-
 const newBook = document.getElementById("new-book");
 const form_tile = document.getElementById("form-tile");
 const form = document.getElementById("form");
@@ -38,14 +24,27 @@ form.addEventListener('submit', (e) => {
     let author = document.getElementById("form-author");
     let pages = document.getElementById("form-pages");
     let read = document.querySelector("input[name='read']:checked");
-
-    console.log(title.value);
-    console.log(author.value);
-    console.log(pages.value);
-    console.log(read.value);
+    
+    const newBook = new Book(title.value, author.value,
+        pages.value, read.value);
+        
+    addBookToLibrary(newBook);
+        
     form_tile.style.display = "none";
     form.reset();
-
-
+    
     return;
 });
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    return;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    return;
+}
