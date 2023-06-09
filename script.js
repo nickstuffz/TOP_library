a54951e9a3860b500d4283d65d65928603dbdaca
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2023-06-08 21:47:45 -0700

    Complete app functionality

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let myLibrary = [];
 
-const books_grid = document.getElementById("books-grid");
-const new_book = document.getElementById("new-book-btn");
-const form_tile = document.getElementById("form-tile");
-const form = document.getElementById("form");
-const close = document.getElementById("close-btn");
+let books_grid = document.getElementById("books-grid");
+let new_book = document.getElementById("new-book-btn");
+let form_tile = document.getElementById("form-tile");
+let form = document.getElementById("form");
+let close = document.getElementById("close-btn");
 
 
 new_book.addEventListener('click', () => {
@@ -25,7 +25,7 @@ form.addEventListener('submit', (e) => {
     let pages = document.getElementById("pages-form");
     let read = document.querySelector("input[name='read']:checked");
     
-    const newBook = new Book(title.value, author.value,
+    let newBook = new Book(title.value, author.value,
         pages.value, read.value);
         
     addBookToLibrary(newBook);
@@ -56,12 +56,55 @@ function displayBooks() {
     while (book_tiles[0]) {
         book_tiles[0].parentNode.removeChild(book_tiles[0]);
     }
-    myLibrary.forEach((book) => {
-        const newTile = document.createElement("div");
-        newTile.setAttribute("class", "tile book-tile");
-        newTile.innerHTML = book.title;
-        books_grid.appendChild(newTile);
+    myLibrary.forEach((book, index) => {
+        let newTile = document.createElement("div");
+        newTile.className = "tile book-tile";
 
+        let newTitle = document.createElement("h2");
+        newTitle.innerHTML = book.title;
+
+        let newAuthor = document.createElement("h3");
+        newAuthor.innerHTML = "by " + book.author;
+
+        let newPages = document.createElement("h4");
+        newPages.innerHTML = book.pages + " pages";
+
+        let newRead = document.createElement("button");
+        if (book.read === "yes") {
+            newRead.innerHTML = "already read";
+            newRead.className = "read-btn";
+            newTile.classList.add("read");
+        }
+        else {
+            newRead.innerHTML = "not yet read";
+            newRead.className = "unread-btn";
+            newTile.classList.add("unread");
+        }
+        newRead.addEventListener('click', () => {
+            if (book.read === "yes") {
+                book.read = "no";
+            }
+            else {
+                book.read = "yes";
+            }
+            displayBooks();
+            console.log(myLibrary);
+            return;
+        });
+
+        let remove_button = document.createElement("button");
+        remove_button.className = "remove-btn";
+        remove_button.innerHTML = "remove";
+        remove_button.addEventListener('click', () => {
+            myLibrary.splice(index, 1);
+            displayBooks();
+        });
+
+        books_grid.appendChild(newTile)
+        .append(newTitle, newAuthor, newPages, newRead, remove_button);
+        
+        return;
+     
     });
 
 }
